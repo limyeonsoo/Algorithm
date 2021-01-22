25c994c041113d9a6f6136ac1d4ffe3dbe49f23b
yeonsoo_lim <yslim6168@naver.com>
2021-01-22 15:42:35 +0900

    LeetCode 124 Binary Tree Maximum Path Sum

--- Tree/LeetCode 124 Binary Tree Maximum Path Sum.cpp
+++ Tree/LeetCode 124 Binary Tree Maximum Path Sum.cpp
@@ -4,48 +4,17 @@ class Solution {
 public:
     int maximum;
     int dfs(TreeNode* now) {
-        int total = 0;
 
-        int left = 0, midLeft = 0;
-        int right = 0, midRight = 0;
-        int midLeftRight = 0;
         int mid = now->val;
-        if (now->left) {
-            left = dfs(now->left);
-            midLeft = left + mid;
-        }
-        else {
-            left = -2147483648;
-            midLeft = -2147483648;
-        }
+        int midLeft = (now->left ? dfs(now->left) + mid : -2147483648);
+        int midRight = (now->right ? dfs(now->right) + mid : -2147483648);
+        int midLeftRight = (now->left && now->right ? midLeft + midRight - mid : -2147483648);
 
-        if (now->right) {
-            right = dfs(now->right);
-            midRight = right + mid;
-        }
-        else {
-            right = -2147483648;
-            midRight = -2147483648;
-        }
-
-        if (now->left && now->right) {
-            midLeftRight = left + mid + right;
-        }
-        else {
-            midLeftRight = -2147483648;
-        }
-
-        int currentMax = max(midLeftRight, max(mid, max(left, max(midLeft, max(right, midRight)))));
+        int currentMax = max(midLeftRight, max(mid, max(midLeft, midRight)));
         maximum = (maximum < currentMax ? currentMax : maximum);
-        if (currentMax == mid || currentMax == midRight || currentMax == midLeft) {
-            return currentMax;
-        }
-        else if (currentMax == midLeftRight) {
-            return max(midLeft, midRight);
-        }
-        else {
-            return max(currentMax + mid, mid);
-        }
+
+        if (currentMax == midLeftRight) return max(midLeft, midRight);
+        return currentMax;
     }
 
     int maxPathSum(TreeNode* root) {

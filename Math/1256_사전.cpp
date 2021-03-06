61eb651665f72d748288ea04fc90fd94a7e3bac8
limyeonsoo <yslim6168@naver.com>
2021-03-06 22:52:53 +0900

    사전

--- Math/1256_사전.cpp
+++ Math/1256_사전.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <algorithm>
 #define ll long long
-#define LIMIT 1000000001
+#define LIMIT 1000000000
 
 using namespace std;
 int N, M, K;
@@ -19,29 +19,38 @@ void init_nCr(){
     for(int i=1; i<=N+M; i++){
         nCr[i][0] = 1;
         for(int j=1; j<=i; j++){
-            nCr[i][j] = min(LIMIT, nCr[i-1][j] + nCr[i-1][j-1]);
+            //nCr[i][j] = min(LIMIT, nCr[i-1][j] + nCr[i-1][j-1]);
+            nCr[i][j] = nCr[i - 1][j] + nCr[i - 1][j - 1];
+            if (nCr[i][j] > LIMIT) nCr[i][j] = LIMIT;
         }
     }
 }
 
-void check(){
-    K--;
-    for(int i= N+M-1; i>=0; i--){
-        if(i >= M && nCr[i][M] > K){
-            cout << "a";
-        }else{   // z가 앞에 나올때마다 숫자는 커진다.
-            K -= nCr[i][M]; // z가 나왔으면 그에 해당하는 큰 수만큼은 다 빠진다.
+string check(){
+    string answer;
+    while (M>0 && N>0) {
+        /*cout << "N : " << N << ' ' << "M : " << M << '\n';
+        cout << nCr[N + M][M] << ' ' << nCr[N + M - 1][M - 1] << "  :  " << K << '\n';
+        cout << nCr[N + M][M] - nCr[N + M - 1][M - 1] << '\n';*/
+        //if (nCr[N + M][M] - nCr[N + M - 1][M - 1] >= K) {
+        if(nCr[N+M-1][M] >= K){
+            answer += "a";
+            N--;
+        }
+        else {
+            answer += "z";
+            //K -= nCr[N + M][M] - nCr[N + M - 1][M - 1];
+            K -= nCr[N + M - 1][M];
             M--;
-            cout << "z";
         }
     }
+    for (int i = 0; i < M; i++) answer += "z";
+    for (int i = 0; i < N; i++) answer += "a";
+    return answer;
 }
 int main(){
     input();
     init_nCr();
-    if(nCr[N+M][M] <= K) cout << "-1";
-    else check();
+    if (nCr[N + M][M] < K) cout << "-1" << '\n';
+    else cout << check() <<'\n';
 }
-
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazazzzzzzzzzzzzzzzzzzazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzazzzzzzzaazzzzzzzzzzzzzzazzzzzzzzzzzzzzz
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazazzzzzzzzzzzzzzzzzzazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzazzzzzzzaazzzzzzzzzzzzzzazzzzzzzzzzzzzzz
